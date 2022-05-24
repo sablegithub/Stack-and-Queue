9bfeb07a5fe08c9ee26557759a3f234ebc0d2d5f
vaibhav sable <vaibhavsable52@gmail.com>
2022-05-24 06:47:59 -0700

    Perform Stack Operation

---
+++ StackExp/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExp
+{
+    public class Stack
+    {
+        Node top;
+        public Stack()
+        {
+            top = null;
+        }
+        public void push(int data)
+        {
+            Node newnode = new Node(data);
+            newnode.next = top;
+            top = newnode;
+        }
+
+        public  void pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Console.WriteLine("\n");
+            this.top = this.top.next;
+        }
+        public void peek()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Top element is: " + top.data);
+            }
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.Write("| " + temp.data + " |");
+                temp = temp.next;
+            }
+        }
+    }
+}
